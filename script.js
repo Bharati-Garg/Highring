cf5e107b1409fe2593aa7b8708e49524e8985d59
Bharati Garg <bharatigargkls1999@gmail.com>
2024-12-11 13:24:12 +0530

    create this webpage

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    rows: 2,
+    dots: false,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
